999ec39f30d380e8bdc113ab548e81c7f7f9e2ad
samuelmarnatti <samuelmarnatti@gmail.com>
2025-04-09 07:44:27 -0300

    fazendo splintter dos dados

--- src/loader.ts
+++ src/loader.ts
@@ -1,8 +1,8 @@
 import path from "node:path";
 
-import  { DiretoryLoader} from "langchain/document_loaders/fs/directory";
+import  { DirectoryLoader} from "langchain/document_loaders/fs/directory";
 import  { JSONLoader} from "langchain/document_loaders/fs/json";
-import { TolkenTextSplitter } from "langchain/text_splitter";
+import { TokenTextSplitter } from "langchain/text_splitter";
 // import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
 
 // /**
@@ -13,7 +13,7 @@ import { TolkenTextSplitter } from "langchain/text_splitter";
 
 // const docs = await loader.load();
 
-const loader = new DiretoryLoader(
+const loader = new DirectoryLoader(
   path.resolve(__dirname,  '../tmp'),
   {
     '.json': path => new JSONLoader(path, '/text')
@@ -22,11 +22,12 @@ const loader = new DiretoryLoader(
 
 async function load() {
   const docs = await loader.load()
-  const splitter = new TolkenTextSplitter({
+  const splitter = new TokenTextSplitter({
       encodingName: 'cl100k_base',
-      chunkSIze:600,
-      chunkOverLap: 0,
+      chunkSize:600,
+      chunkOverlap: 0,
   })
   const splittedDocuments = await splitter.splitDocuments(docs)
+  console.log(splittedDocuments);
 }
 load()
