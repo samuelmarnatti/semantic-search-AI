5b511446041b20ad5a09ffafa7666338e06fd984
samuelmarnatti <samuelmarnatti@gmail.com>
2025-05-19 12:14:23 -0300

    usando Qdrant e funcionando perfeitamente

--- scripts/api.py
+++ scripts/api.py
@@ -6,10 +6,10 @@ model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 @app.route('/embed', methods=['POST'])
 def embed():
-    # data = request.get_json()
-    # sentences = data.get('sentences', [])
-    # embeddings = model.encode(sentences)
-    return jsonify('isso é um texto teste')
+    data = request.get_json()
+    sentences = data.get('sentences', [])
+    embeddings = model.encode(sentences)
+    return jsonify(embeddings.tolist())
 
 if __name__ == '__main__':
     app.run(port=5000)
--- src/loader.ts
+++ src/loader.ts
@@ -1,57 +1,44 @@
 import path from "node:path";
+import { Qdrant } from 'qdrant';
 import  { DirectoryLoader} from "langchain/document_loaders/fs/directory";
 import  { JSONLoader} from "langchain/document_loaders/fs/json";
 import { TokenTextSplitter } from "langchain/text_splitter";
-import { RedisVectorStore } from "@langchain/redis";
-import { createClient } from 'redis';
+import { QdrantVectorStore } from "@langchain/qdrant";
 import { HuggingFaceEmbeddingsAdapter } from "./huggingFaceEmbeddingsAdapter";
+import { QdrantClient } from "@qdrant/js-client-rest";
 
-const loader = new DirectoryLoader(
-  path.resolve(__dirname, "../tmp"),
-  {
-    ".json": (path) => new JSONLoader(path, "/text"),
-  }
-);
+  const loader = new DirectoryLoader(
+    path.resolve(__dirname, "../tmp"),
+    {
+      ".json": (path) => new JSONLoader(path, "/text"),
+    }
+  );
 
-async function load() {
-  const docs = await loader.load();
-  const splitter = new TokenTextSplitter({
-    encodingName: "cl100k_base",
-    chunkSize: 600,
-    chunkOverlap: 0,
+  const qdrant = new QdrantClient({
+    url: "http://127.0.0.1:6333",
   });
-  const splittedDocuments = await splitter.splitDocuments(docs);
-
-  const redis = createClient({
-    url: "redis://127.0.0.1:6379",
-  });
-  try {
-    await redis.connect();
-    console.log("✅ Conectado ao Redis com sucesso!");
-  
-    // Testa se consegue executar um comando simples
-    await redis.set("teste_conexao", "ok");
-    const valor = await redis.get("teste_conexao");
-    console.log("🔍 Teste de conexão Redis:", valor); // deve exibir "ok"
-  } catch (error) {
-    console.error("❌ Erro ao conectar com o Redis:", error);
-    process.exit(1); // encerra o script se falhar
-  }
-  try {
-    const info = await redis.sendCommand(['FT.INFO', 'textos-embeddings']);
-    console.log('FT.INFO manual:', info);
-  } catch (err) {
-    console.error('Erro ao testar FT.INFO manual:', err);
-  }
-
   const embeddings = new HuggingFaceEmbeddingsAdapter();
-  await RedisVectorStore.fromDocuments(splittedDocuments, embeddings, {
-    redisClient: redis,
-    indexName: "textos-embeddings",
-    keyPrefix: "texto:",
-  });
+  // const texts = [
+  //   "LangChain facilita a construção de apps com IA.",
+  //   "Qdrant é um banco de vetores para buscas semânticas.",
+  // ];
+  async function importar() {
+    const docs = await loader.load();
 
-  await redis.disconnect();
-}
+    const splitter = new TokenTextSplitter({
+      encodingName: "cl100k_base",
+      chunkSize: 600,
+      chunkOverlap: 0,
+    });
+    const splittedDocuments = await splitter.splitDocuments(docs);
 
-load();
+    const vectorStore = await QdrantVectorStore.fromDocuments(splittedDocuments, embeddings, {
+        client: qdrant,
+        collectionName: "igreja_episcopal",
+      });
+    
+    console.log("Importação concluída!");
+  }
+  importar().catch(console.error);
+
+
